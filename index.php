68a4c51f8c7b546e0ba8e439f6aad16df8d6481c
Jürgen Müller <j.mueller6@gmx.net>
2024-05-21 19:51:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 include('./latin2utf8.php');
 
 $latin2Utf8 = new Latin2Utf8("wp_test", "jonny", "root");
-//$latin2Utf8->addSearchReplace("farfallina.local", "www.farfallina.info");
+//$latin2Utf8->addSearchReplace("develop.local", "www.develop.de");
 $latin2Utf8->updateTables(false);
