2cb34c225e2374e0e49b81404c3c0e68ceef1c6a
Jürgen Müller <j.mueller6@gmx.net>
2024-05-23 09:07:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 include('./latin2utf8.php');
 
-$latin2Utf8 = new Latin2Utf8("wp_test", "jonny", "root");
+$database = "wp_test"; // e.g.
+$username = "jonny";   // e.g.
+$password = "root";    // e.g.
+
+$latin2Utf8 = new Latin2Utf8($database, $username, $password);
 //$latin2Utf8->addSearchReplace("develop.local", "www.develop.de");
 $latin2Utf8->updateTables(false);
